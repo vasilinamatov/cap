d264e8b370ddd370cd9788394f64a21abef3bff9
vnamatov <vnamatov@liveperson.com>
2021-01-07 21:42:51 +0200

    remove file

--- deploy-jenkins.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#JENKINS INSTALL
-docker pull jenkins/jenkins
-docker run -d -p 8080:8080 --name=jenkins-master jenkins/jenkins
-docker logs jenkins-master
-#get the administrator password for jenkins access modification
